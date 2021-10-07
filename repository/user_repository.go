b75d4da15588b9a1668703d8307fb86a8a0abca8
rizqito <qito25@gmail.com>
2021-10-07 21:36:08 +0800

    feat: add create user API :sparkles:

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"backend-c-payment-monitoring/config"
 	"backend-c-payment-monitoring/model"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func FindAllUser(pagination model.Pagination) (model.Pagination, error) {
@@ -35,6 +37,27 @@ func FindUserByUsername(username string) (model.User, error) {
 	var user model.User
 	err := db.Preload("Role").Where("username = ?", username).First(&user).Error
 
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func CreateUser(payload model.User) (model.User, error) {
+	configuration := config.New()
+	db := config.NewMysqlDatabase(configuration)
+
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+
+	var user = model.User{
+		Name:     payload.Name,
+		Username: payload.Username,
+		Password: string(passwordHash),
+		RoleID:   payload.RoleID,
+	}
+
+	err := db.Save(&user).Error
 	if err != nil {
 		return user, err
 	}
